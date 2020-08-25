50aa7dffb20ceec3dc2beb0ffa83d637e0736ff8
David Marquardt <davemarquardt@gmail.com>
2020-08-25 13:26:20 -0500

    Added getters, made fields private

--- src/main/java/edu/wctc/reporting/Sale.java
+++ src/main/java/edu/wctc/reporting/Sale.java
@@ -2,15 +2,31 @@ package edu.wctc.reporting;
 
 public class Sale {
 
-    String name;
-    String country;
-    double amount;
-    double tax;
+    private String name;
+    private String country;
+    private double amount;
+    private double tax;
 
     public Sale(String name, String country, double amount, double tax) {
         this.name = name;
         this.country = country;
         this.amount = amount;
         this.tax = tax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getTax() {
+        return tax;
     }
 }
