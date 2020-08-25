5e4cb1798bd531e673ce383da0815ed4b42160e9
David Marquardt <davemarquardt@gmail.com>
2020-08-25 09:25:40 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/reporting/App.java
@@ -0,0 +1,11 @@
+package edu.wctc.reporting;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+    }
+}
---
+++ src/main/java/edu/wctc/reporting/AppConfig.java
@@ -0,0 +1,10 @@
+package edu.wctc.reporting;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("edu.wctc.reporting")
+public class AppConfig {
+
+}
