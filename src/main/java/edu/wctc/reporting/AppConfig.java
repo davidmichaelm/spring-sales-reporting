7e6fc96f901434cef09319dba170d25202d6b8e1
David Marquardt <davemarquardt@gmail.com>
2020-08-25 09:47:30 -0500

    Set up AppConfig

--- src/main/java/edu/wctc/reporting/AppConfig.java
+++ src/main/java/edu/wctc/reporting/AppConfig.java
@@ -1,5 +1,10 @@
 package edu.wctc.reporting;
 
+import edu.wctc.reporting.iface.SalesInput;
+import edu.wctc.reporting.iface.SalesReport;
+import edu.wctc.reporting.iface.ShippingPolicy;
+import edu.wctc.reporting.impl.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,4 +12,22 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("edu.wctc.reporting")
 public class AppConfig {
 
+    @Bean
+    SalesInput salesInput() {
+        return new ConsoleSalesInput();
+        //return new FileSalesInput();
+    }
+
+    @Bean
+    SalesReport salesReport() {
+        return new CountrySalesReport();
+//        return new DetailedSalesReport();
+    }
+
+    @Bean
+    ShippingPolicy shippingPolicy() {
+        return new FreeShippingPolicy();
+//        return new FlatRateDomesticShippingPolicy();
+//        return new FreeShippingOverXShippingPolicy();
+    }
 }
