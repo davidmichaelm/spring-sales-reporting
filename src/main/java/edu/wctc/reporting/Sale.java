8ad67a3da74ce3a6a5a1d5ac6a189c4feb42cf29
David Marquardt <davemarquardt@gmail.com>
2020-08-25 10:48:44 -0500

    Added ConsoleSalesInput implementation

--- src/main/java/edu/wctc/reporting/Sale.java
+++ src/main/java/edu/wctc/reporting/Sale.java
@@ -1,4 +1,16 @@
 package edu.wctc.reporting;
 
 public class Sale {
+
+    String name;
+    String country;
+    double amount;
+    double tax;
+
+    public Sale(String name, String country, double amount, double tax) {
+        this.name = name;
+        this.country = country;
+        this.amount = amount;
+        this.tax = tax;
+    }
 }
--- src/main/java/edu/wctc/reporting/impl/ConsoleSalesInput.java
+++ src/main/java/edu/wctc/reporting/impl/ConsoleSalesInput.java
@@ -3,12 +3,99 @@ package edu.wctc.reporting.impl;
 import edu.wctc.reporting.Sale;
 import edu.wctc.reporting.iface.SalesInput;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ConsoleSalesInput implements SalesInput {
 
+    Scanner keyboard;
+
+    public ConsoleSalesInput() {
+        keyboard = new Scanner(System.in);
+    }
+
     @Override
     public List<Sale> getSales() {
-        return null;
+        List<Sale> sales = new ArrayList<>();
+
+        do {
+            String name = getName();
+            String country = getCountry();
+            double amount = getAmount();
+            double tax = getTax();
+
+            Sale sale = new Sale(name, country, amount, tax);
+            sales.add(sale);
+
+        } while (getAddMore());
+
+        return sales;
+    }
+
+    private String getName() {
+        System.out.println("Enter the name:");
+        return getString();
+    }
+
+    private String getCountry() {
+        System.out.println("Enter the country:");
+        return getString();
+    }
+
+    private double getAmount() {
+        System.out.println("Enter the amount of the sale:");
+        return getDouble();
+    }
+
+    private double getTax() {
+        System.out.println("Enter the tax:");
+        return getDouble();
+    }
+
+    private String getString() {
+        String input = keyboard.nextLine();
+        while (input.isEmpty()) {
+            System.out.println("Input must be at least one character. Try again:");
+            input = keyboard.nextLine();
+        }
+
+        return input;
+    }
+
+    private double getDouble() {
+        double parsedDouble = 0.0;
+        boolean parsed = false;
+        do {
+            try {
+                String amount = keyboard.nextLine();
+                parsedDouble = Double.parseDouble(amount);
+
+                if (parsedDouble >= 0) {
+                    parsed = true;
+                } else {
+                    System.out.println("Input cannot be negative. Try again:");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Input must be a double. Try again:");
+            } catch (NullPointerException e) {
+                System.out.println("Input must not be null. Try again:");
+            }
+        } while (!parsed);
+
+        return parsedDouble;
+    }
+
+    private boolean getAddMore() {
+        System.out.println("Add another sale? y\\n");
+        String input = getString().toUpperCase();
+
+        while (!input.equals("Y") && !input.equals("N")) {
+            System.out.println("Enter 'y' or 'n':");
+            input = getString().toUpperCase();
+        }
+
+        return input.equals("Y");
     }
 }
